d20921d3680fae0f3dc246a80c0e2d7584b7d58d
Ivancozy <61648574+Ivancozy@users.noreply.github.com>
2023-03-01 09:53:51 +0000

    🐛 BUG: fix PWA Updated By Github Actions

--- js/aplayer.js
+++ js/aplayer.js
@@ -1,18 +1,1 @@
-function removelrc() {
-    //检测是否存在歌词按钮
-    if (!document.querySelector(".aplayer-icon-lrc"))
-        return;
-    else
-    {
-        //触发以后立刻移除监听
-        document.removeEventListener("DOMNodeInserted",removelrc);
-        //稍作延时保证触发函数时存在按钮
-        setTimeout(function() {
-            //以触发按钮的方式隐藏歌词，防止在点击显示歌词按钮时需要点击两次才能出现的问题
-            document.querySelector(".aplayer-icon-lrc").click();
-        }, 1);
-        console.log("歌词隐藏success");
-        return;
-    }
-}
-document.addEventListener('DOMNodeInserted', removelrc)
+function removelrc(){return document.querySelector(".aplayer-icon-lrc")?(document.removeEventListener("DOMNodeInserted",removelrc),setTimeout((function(){document.querySelector(".aplayer-icon-lrc").click()}),1),void console.log("歌词隐藏success")):void 0}document.addEventListener("DOMNodeInserted",removelrc);
--- js/ivanyfunction.js
+++ js/ivanyfunction.js
@@ -1,9 +1,1 @@
-// 音乐绑定事件
-function musicBindEvent() {
-  document.querySelector("#nav-music .aplayer-music").addEventListener("click", function () {
-    ivany.musicTelescopic();
-  });
-  document.querySelector("#nav-music .aplayer-button").addEventListener("click", function () {
-    ivany.musicToggle(false);
-  });
-}
+function musicBindEvent(){document.querySelector("#nav-music .aplayer-music").addEventListener("click",(function(){ivany.musicTelescopic()})),document.querySelector("#nav-music .aplayer-button").addEventListener("click",(function(){ivany.musicToggle(!1)}))}
